3aa03106555c1300d754365bab16db59a6d3fd73
Nandeesh778 <nandeeshv0001@gmail.com>
2026-08-06 17:11:47 +0530

    Fixed: Email configuration issue, App launch logo placement and New Button on settings screen.

--- screens/AppLayoutController.ts
+++ screens/AppLayoutController.ts
@@ -1,19 +1,23 @@
-import { useSelector } from '@xstate/react';
-import { useContext } from 'react';
+import {useSelector} from '@xstate/react';
+import {useContext} from 'react';
 import {
   selectAuthorized,
+  selectIntroSlider,
   selectLanguagesetup,
+  selectSettingUp,
   selectUnauthorized,
 } from '../machines/auth';
-import { GlobalContext } from '../shared/GlobalContext';
+import {GlobalContext} from '../shared/GlobalContext';
 
 export function useAppLayout() {
-  const { appService } = useContext(GlobalContext);
+  const {appService} = useContext(GlobalContext);
   const authService = appService.children.get('auth');
-  const isLanguagesetup = useSelector(authService, selectLanguagesetup);
+
   return {
     isAuthorized: useSelector(authService, selectAuthorized),
     isUnAuthorized: useSelector(authService, selectUnauthorized),
-    isLanguagesetup,
+    isSettingUp: useSelector(authService, selectSettingUp),
+    isLanguagesetup: useSelector(authService, selectLanguagesetup),
+    isIntroSlider: useSelector(authService, selectIntroSlider),
   };
 }
